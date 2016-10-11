4e246e38aaf3e685e161f5cadbfe0c558b5ee6c2
diluculo <diluculo@gmail.com>
2016-10-11 16:36:09 +0900

    Add Label to EditorFileType

--- src/Gemini/Framework/Services/EditorFileType.cs
+++ src/Gemini/Framework/Services/EditorFileType.cs
@@ -2,11 +2,20 @@
 {
     public class EditorFileType
     {
+        public string Label { get; set; }
         public string Name { get; set; }
         public string FileExtension { get; set; }
 
         public EditorFileType(string name, string fileExtension)
         {
+            Label = name;
+            Name = name;
+            FileExtension = fileExtension;
+        }
+
+        public EditorFileType(string label, string name, string fileExtension)
+        {
+            Label = label;
             Name = name;
             FileExtension = fileExtension;
         }
