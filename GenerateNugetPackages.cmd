9bb64309d99095e586aac40a1ebc0112a9bcbd0b
diluculo <diluculo@gmail.com>
2019-11-20 14:00:52 +0900

    Add GenerateNugetPackages.cmd

---
+++ GenerateNugetPackages.cmd
@@ -0,0 +1,14 @@
+
+nuget.exe pack src\Gemini\Gemini.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.CodeCompiler\Gemini.Modules.CodeCompiler.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.CodeEditor\Gemini.Modules.CodeEditor.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.ErrorList\Gemini.Modules.ErrorList.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.GraphEditor\Gemini.Modules.GraphEditor.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.Inspector\Gemini.Modules.Inspector.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.Inspector.MonoGame\Gemini.Modules.Inspector.MonoGame.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.Inspector.Xna\Gemini.Modules.Inspector.Xna.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.MonoGame\Gemini.Modules.MonoGame.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.Output\Gemini.Modules.Output.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.PropertyGrid\Gemini.Modules.PropertyGrid.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.SharpDX\Gemini.Modules.SharpDX.csproj -OutputDirectory out
+nuget.exe pack src\Gemini.Modules.Xna\Gemini.Modules.Xna.csproj -OutputDirectory out
